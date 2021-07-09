372394cca6c4605109e3e77c0dc1f63768b50074
Sumeetha <sumeethakota@gmail.com>
2021-07-09 22:09:28 +0530

    Added username and password inside login form.

--- src/common/header/Header.js
+++ src/common/header/Header.js
@@ -3,13 +3,41 @@ import './Header.css';
 import Button from '@material-ui/core/Button';
 import logo from '../../assets/logo.svg';
 import Modal from 'react-modal';
+import Tabs from '@material-ui/core/Tabs';
+import Tab from '@material-ui/core/Tab';
+import TableContainer from '@material-ui/core/TableContainer';
+import Typography from '@material-ui/core/Typography';
+import FormControl from '@material-ui/core/FormControl';
+import  InputLabel  from '@material-ui/core/InputLabel';
+import  Input  from '@material-ui/core/Input';
+
+const CustomStyles = {
+    content: {
+        top: '50%',
+        left: '50%',
+        right: 'auto',
+        bottom: 'auto',
+        marginRight: '-50%',
+        transform: 'translate(-50%, -50%)'
+    }
+
+};
+
+const TabContainer = function(props) {
+    return(
+        <Typography component="div" style={{padding: 0}}>
+            {props.children}
+        </Typography>
+    );
+}
 
 class Header extends Component {
     constructor()
     {
         super();
         this.state = {
-            modalIsOpen: false
+            modalIsOpen: false,
+            value: 0
         }
     }
 
@@ -21,6 +49,9 @@ class Header extends Component {
         this.setState({ modalIsOpen: false});
     }
 
+    tabChangeHandler = (event,value) => {
+        this.setState({value});
+    }
 
     render() {
         return (
@@ -34,7 +65,24 @@ class Header extends Component {
                     </div>
                 </header>
                 <Modal ariaHideApp={false}  isOpen={this.state.modalIsOpen} contentLabel="Login"
-                onRequestClose={this.closeModalHandler}>
+                onRequestClose={this.closeModalHandler}
+                style={CustomStyles}
+                >
+                    <Tabs value={this.state.value} onChange={this.tabChangeHandler}>
+                        <Tab label="Login"/>
+                        <Tab label="Register"/>
+                    </Tabs>
+                    <TableContainer>
+                        <FormControl required>
+                            <InputLabel htmlFor="userName"> Username </InputLabel>
+                            <Input id="username" type="text" />
+                        </FormControl>
+
+                        <FormControl required>
+                            <InputLabel htmlFor="password">Password</InputLabel>
+                            <Input id="password" type="Password"  />
+                        </FormControl>
+                    </TableContainer>
 
                 </Modal>
             </div>
--- src/index.js
+++ src/index.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import 'typeface-roboto';
 import './index.css';
 import reportWebVitals from './reportWebVitals';
 import Home from '../src/screens/home/Home';
