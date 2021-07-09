9cf8eaea885f0279e628ebbaad280f37db6801cf
Sumeetha <sumeethakota@gmail.com>
2021-07-09 20:22:59 +0530

    Added Logo.

--- src/common/header/Header.js
+++ src/common/header/Header.js
@@ -1,12 +1,20 @@
 import React, { Component } from 'react';
 import './Header.css';
 import Button from '@material-ui/core/Button';
+import logo from '../../assets/logo.svg';
 
 class Header extends Component {
     render() {
         return (
             <div>
-                <Button variant="contained" color="default">Login</Button>
+                <header className="header-A">
+                <img src={logo} className="app-logo" alt="logo" />
+                    <div className="login-but">
+                        <Button variant="contained" color="default">
+                            Login
+                        </Button>
+                    </div>
+                </header>
             </div>
         )
     }
