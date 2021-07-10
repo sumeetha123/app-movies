090a4d24df02be8392162075d74e0dde7dce6fc1
Sumeetha <sumeethakota@gmail.com>
2021-07-10 20:47:13 +0530

    Added 'Movie Name' filter on home page.

--- src/screens/home/Home.js
+++ src/screens/home/Home.js
@@ -6,7 +6,12 @@ import moviesData from '../../common/movieData';
 import ImageList from '@material-ui/core/ImageList';
 import ImageListItem from '@material-ui/core/ImageListItem';
 import ImageListItemBar from '@material-ui/core/ImageListItemBar';
-
+import Card from '@material-ui/core/Card';
+import CardContent  from '@material-ui/core/CardContent';
+import FormControl from '@material-ui/core/FormControl';
+import  Typography  from '@material-ui/core/Typography';
+import InputLabel from '@material-ui/core/InputLabel';
+import Input from '@material-ui/core/Input';
 
 const styles = theme => ({
     root: {
@@ -23,10 +28,37 @@ const styles = theme => ({
         flexWrap: 'nowrap',
         transform: 'translateZ(0)',
         width: '100%'
-    }
+    },
+
+    ImageListMain: {
+        transform: 'translateZ(0)',
+        cursor: 'pointer'
+    },
+
+    formControl: {
+        margin: theme.spacing.unit,
+        minWidth: 240,
+        maxWidth: 240
+     },
+
+     title: {
+        color: theme.palette.primary.light,
+     }
  });
 
 class Home extends Component {
+
+    constructor() {
+        super();
+        this.state = {
+            movieName: ""
+        }
+    }
+
+    movieNameChangeHandler = event => {
+        this.setState({ movieName: event.target.value });
+    }
+    
     render() {
         const { classes } = this.props;
         return (
@@ -43,6 +75,37 @@ class Home extends Component {
                         </ImageListItem>
                     ))}
                 </ImageList>
+
+                <div className="flex-container">
+                    <div className="left">
+                        <ImageList cellHeight={350} cols={4} className={classes.ImageListMain}>
+                            {moviesData.map(movie => (
+                                <ImageListItem className="released-movie-grid-item" key={"grid" + movie.id}>
+                                    <img src={movie.poster_url} className="movie-poster" alt={movie.title} />
+                                    <ImageListItemBar
+                                        title={movie.title}
+                                        subtitle={<span>Release Date: {new Date(movie.release_date).toDateString()}</span>}
+                                    />
+                                </ImageListItem>
+                            ))}
+                        </ImageList>
+                    </div>
+                    <div className="right">
+                        <Card>
+                            <CardContent>
+                                <FormControl className={classes.formControl}>
+                                    <Typography className={classes.title} color="textSecondary">
+                                        FIND MOVIES BY:
+                                    </Typography>
+                                </FormControl>
+                                <FormControl className={classes.formControl}>
+                                    <InputLabel htmlFor="movieName">Movie Name</InputLabel>
+                                    <Input id="movieName" onchange={this.movieNameChangeHandler}  />
+                                </FormControl>
+                            </CardContent>
+                        </Card>
+                    </div>
+                </div>
             </div>
         )
     }
