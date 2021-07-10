9bc6de2d24d9f9b0bb2bf85ac5e78632cc70c7ca
Sumeetha <sumeethakota@gmail.com>
2021-07-10 22:47:03 +0530

    Added Apply button

--- src/screens/home/Home.js
+++ src/screens/home/Home.js
@@ -19,6 +19,7 @@ import MenuItem from '@material-ui/core/MenuItem';
 import Checkbox from '@material-ui/core/Checkbox';
 import ListItemText from '@material-ui/core/ListItemText';
 import TextField from '@material-ui/core/TextField';
+import Button from '@material-ui/core/Button';
 
 
 const styles = theme => ({
@@ -165,6 +166,21 @@ class Home extends Component {
                                         defaultValue=""
                                         InputLabelProps={{ shrink: true }}
                                     />
+                                </FormControl>
+                                <FormControl className={classes.formControl}>
+                                    <TextField
+                                        id="releaseEndStart"
+                                        label="Release End Start"
+                                        type="date"
+                                        defaultValue=""
+                                        InputLabelProps={{ shrink: true }}
+                                    />
+                                </FormControl>
+                                <br /><br />
+                                <FormControl className={classes.formControl}>
+                                <Button variant="contained" color="primary">
+                                    APPLY
+                                </Button>
                                 </FormControl>
                             </CardContent>
                         </Card>
