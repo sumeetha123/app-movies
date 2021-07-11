5802773c9729c1a2fb177e4ee9917d11421a8bb9
Sumeetha <sumeethakota@gmail.com>
2021-07-11 10:56:28 +0530

    Added Book show button and assigned page to it

--- src/common/header/Header.js
+++ src/common/header/Header.js
@@ -1,4 +1,5 @@
 import React, { Component } from 'react';
+import ReactDOM from 'react-dom';
 import './Header.css';
 import Button from '@material-ui/core/Button';
 import logo from '../../assets/logo.svg';
@@ -12,6 +13,7 @@ import  InputLabel  from '@material-ui/core/InputLabel';
 import  Input  from '@material-ui/core/Input';  
 import PropTypes from 'prop-types';
 import FormHelperText from '@material-ui/core/FormHelperText';
+import BookShow from '../../screens/bookshow/BookShow';
 
 const CustomStyles = {
     content: {
@@ -144,6 +146,11 @@ class Header extends Component {
         this.setState({ contact: e.target.value });
     }
 
+    bookShowHandler = (e) => {
+        ReactDOM.render(<BookShow />, document.getElementById('root'));
+    }
+
+
 
 
     render() {
@@ -156,6 +163,14 @@ class Header extends Component {
                             Login
                         </Button>
                     </div>
+                    {this.props.showBookShowButton === "true" ?
+                    <div className="bookshow-button">
+                        <Button variant="contained" color="primary" onClick="default" onClick={this.bookShowHandler}>
+                            Book Show
+                        </Button>
+                    </div>
+                    : ""}
+
                 </header>
                 <Modal ariaHideApp={false}  isOpen={this.state.modalIsOpen} contentLabel="Login"
                 onRequestClose={this.closeModalHandler}
